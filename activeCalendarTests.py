0c5148688b01c5f523811a2aa44ddd6d4631f045
drocta <madaco.madaco@gmail.com>
2023-04-09 13:01:44 -0700

    make activeCalendarTests.py use time-machine instead of just mock

--- activeCalendarTests.py
+++ activeCalendarTests.py
@@ -2,6 +2,7 @@
 """unit test for ActiveCalendar"""
 import unittest
 from unittest.mock import MagicMock, patch
+import time_machine
 from activeCalendar import ActiveCalendar
 import icalendar
 import datetime
@@ -22,15 +23,15 @@ class TestActiveCalendar(unittest.TestCase):
     def test_active_events(self):
         calendar = calFromFile(testFileLocation)
 
-        with patch('datetime.datetime') as mock_datetime:
-            mock_datetime.now.return_value = datetime.datetime(2023, 3, 6, 9, 0,tzinfo=pytz.utc)
+        with time_machine.travel(datetime.datetime(2023, 3, 6, 9, 0,tzinfo=pytz.utc),tick=False):
+            #mock_datetime.now.return_value = datetime.datetime(2023, 3, 6, 9, 0,tzinfo=pytz.utc)
             activeCalendar = ActiveCalendar(calendar)
         #check that no events are active at 9:00 AM
         self.assertEqual(activeCalendar.activeEvents,[])
 
         #move the clock to 10:30AM and update the active events:
-        with patch('datetime.dateteime') as mock_datetime:
-            mock_datetime.now.return_value = datetime.datetime(2023, 3, 6, 10, 30,tzinfo=pytz.utc)
+        with time_machine.travel(datetime.datetime(2023, 3, 6, 10, 30,tzinfo=pytz.utc),tick=False):
+            #mock_datetime.now.return_value = datetime.datetime(2023, 3, 6, 10, 30,tzinfo=pytz.utc)
             active, inactive, missed = activeCalendar.wake()
 
         #check that active, inactive, and missed are correct
