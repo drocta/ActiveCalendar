93407915c756eb01b97e7b36cb4e019e0ccf5a27
drocta <madaco.madaco@gmail.com>
2023-08-21 13:50:58 -0700

    update dependencies list in setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,9 @@ setup(
         name="active_calendar",
         version="0.1",
         packages=find_packages(),
-        install_requires=[]
+        install_requires=[
+                'icalendar',
+                'recurring_ical_events',
+                'pytz'
+            ]
         )
